aeb71aa58d2e6135c6d7a7e2d0bccdf30871d2f9
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-03-28 11:05:31 +0300

    feat(login): Implement login logic
    Enable login functionality for existing users.

--- user_mgt_app/urls.py
+++ user_mgt_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.landing, name="user-landing"),
-    path('register', views.register, name="user-register"),
-    path('sign-in', views.sign_in, name="user-sign-in"),
+    path('register/', views.register, name="user-register"),
+    path('sign-in/', views.custom_login, name="user-sign-in"),
+    path('home/', views.home, name="home"),
+    # path('home/', TemplateView.as_view(template_name="user_mgt_app/home.html"), name="home")
 ]
--- user_mgt_app/views.py
+++ user_mgt_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
 from django.http import HttpResponse
 
 def landing(request):
@@ -8,4 +11,19 @@ def register(request):
     return render(request, 'user_mgt_app/register.html')
 
 def sign_in(request):
+    return render(request, 'user_mgt_app/sign_in.html')
+
+def home(request):
+    return render(request, "user_mgt_app/home.html")
+
+def custom_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password.")
     return render(request, 'user_mgt_app/sign_in.html')
